1617b2431ed08f0f9500fc4ed06a95c773cb986b
wangcan <wangcan.1991@bytedance.com>
2025-01-09 01:17:07 +0800

    First Commit

---
+++ app/src/main/java/com/demo/TestFramePlayer.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.demo.view.FrameSurfaceView;
+
+public class TestFramePlayer extends Activity {
+    private FrameSurfaceView mSurfaceView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_frame_player);
+
+        // Example of a call to a native method
+        mSurfaceView = (FrameSurfaceView) findViewById(R.id.frame_surface_view);
+        mSurfaceView.init(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/com/demo/view/FrameSurfaceView.java
@@ -0,0 +1,47 @@
+package com.demo.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.util.AttributeSet;
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.ref.WeakReference;
+
+public class FrameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder       mSurfaceHolder;
+    private WeakReference<Activity>     mWeakActivity;
+
+    public FrameSurfaceView(Context context) {
+        this(context, null);
+    }
+
+    public FrameSurfaceView( Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void init(Activity activity) {
+        mWeakActivity = new WeakReference<>(activity);
+        mSurfaceHolder = this.getHolder();
+        mSurfaceHolder.addCallback(this);
+        mSurfaceHolder.setKeepScreenOn(true);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+}
