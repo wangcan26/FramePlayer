d0e8e13f2ac0e51a75a336cd3c6b52f9168633c4
wangcan <wangcan.1991@bytedance.com>
2025-02-16 19:03:49 +0800

    add main activity and .cursorules

--- FramePlayerNative/source/extra/producer/fpn_gif_producer.cpp
+++ FramePlayerNative/source/extra/producer/fpn_gif_producer.cpp
@@ -121,7 +121,7 @@ namespace fpn
     FPNGifProducer::FPNGifProducer(const std::string& uri, FPNPlayer* player)
     {
         mPlayer = player;
-        mGifFilePath = uri + std::string("/gif/output_540.gif");
+        mGifFilePath = uri + std::string("/gif/testGif.gif");
         mIsRunning = true;
         mDecoderThread = std::thread(&FPNGifProducer::_decode, this);
     }
